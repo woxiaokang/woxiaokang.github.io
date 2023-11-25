e5f624b2c61196a88b463978b27480148338c05e
woxiaokang <47183710+woxiaokang@users.noreply.github.com>
2023-11-25 22:58:56 +0800

    zxk web first
    zxk web first ......

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/floret.png") {
+        myImage.setAttribute("src", "images/floret2.png");
+    } else {
+        myImage.setAttribute("src", "images/floret.png");
+    }
+};
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName;
+    while (!myName) {
+        myName = prompt("请输入你的名字。");
+    }
+    
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+};
